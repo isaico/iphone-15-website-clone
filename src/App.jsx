da2a8048612e44d62760368ba6c137b77771f9bc
Isaias Garcia <82178567+isaico@users.noreply.github.com>
2024-04-16 11:19:48 -0300

    [stup]-finish base, starting comp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+function App() {
+    return (
+        <>
+            <h1 className="font-bold text-3xl"> hello</h1>
+            <Navbar></Navbar>
+            <Hero></Hero>
+            <Highlights></Highlights>
+        </>
+    );
+}
+
+export default App;
